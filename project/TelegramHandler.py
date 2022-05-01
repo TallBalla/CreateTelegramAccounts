7aaa75f8c61331e7c55e4b0850661b1d9001f945
Willem Russ-Hofmans <willrusshofmans@gmail.com>
2022-05-01 22:16:28 +1200

    formating update

---
+++ project/TelegramHandler.py
@@ -0,0 +1,8 @@
+import asyncio
+from telethon import TelegramClient
+
+class TelegramHandler():
+    id = '15075009'
+    hash = '1b3f35183a1865d05a169f8f60106151'
+
+    client = TelegramClient('tester', id, hash)
